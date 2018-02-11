69798b55dc96f914cf54f6c09c7b4ead58c7936e
Olivier Pichon <op@united-asian.com>
2018-02-11 22:19:37 +0700

    updated sidebar toggle

--- src/_store/actions.js
+++ src/_store/actions.js
@@ -1,7 +1,5 @@
 const toggleSidebar = (context) => {
   context.commit('TOGGLE_SIDEBAR')
-
-  localStorage.setItem('uam.layout.sidebar', state.sidebar)
 }
 
 export default {
--- src/_store/mutations.js
+++ src/_store/mutations.js
@@ -1,5 +1,7 @@
 const TOGGLE_SIDEBAR = (state) => {
   state.sidebar = state.sidebar === 'collapsed' ? 'expanded' : 'collapsed'
+
+  localStorage.setItem('uam.layout.sidebar', state.sidebar)
 }
 
 export default {
