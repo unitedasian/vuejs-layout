4625ac26e1aecd14557e58a4197147021e1fd004
Olivier Pichon <op@united-asian.com>
2018-02-15 15:50:24 +0800

    updated scss

--- src/components/SidebarToggle.vue
+++ src/components/SidebarToggle.vue
@@ -25,29 +25,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import "../_scss/_vars";
-
-.toggle {
-  background-color: #fff;
-  border: none;
-  height: 100%;
-  left: 0;
-  padding: 0 0.5rem;
-  position: absolute;
-  top: 350px;
-
-  img {
-    height: 100%;
-    object-fit: contain;
-  }
-}
-
-@media (min-width: 768px) {
-  .toggle {
-    left: 181px;
-    top: 0;
-  }
-}
-</style>
